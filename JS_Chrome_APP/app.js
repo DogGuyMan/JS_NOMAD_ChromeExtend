bee2748a737d3691f13bcc316941841b23a69ce8
escatrgot <escatrgot@gmail.com>
2022-01-13 23:28:13 +0900

    preventDefault() 연습

--- JS_Chrome_APP/app.js
+++ JS_Chrome_APP/app.js
@@ -2,19 +2,27 @@
     // 내부에 있는 다른 element에 접근해보기
 const loginForm = document.querySelector(".login-form");
 //loginForm을 재활용해서 접근할 수도 있다.
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+    //document.querySelector("div.user-panel.main input[name=login]");
+    //document.querySelector("selector[myAttribute='aValue']");
+    //document.querySelectorAll("selector[myAttribute='aValue']");
+const loginInput = loginForm.querySelector("input[type=text]");
+const loginSubmit = loginForm.querySelector("input[type=submit]")
+const blogLink = document.querySelector("a");
 
+console.log(loginForm);
+console.log(loginInput);
+console.dir(loginSubmit);
+console.log(blogLink);
 
-function handleLoginBtnClick(){
-    if(loginInput.value === ""){
-        alert("아이디를 입력해줘");
-    }
-    else {
-        console.log(loginInput.value);
-    }
+function onLoginSubmit(event){
+    event.preventDefault();
+    console.log("YouSubmit");
 }
+loginForm.addEventListener("submit", onLoginSubmit);
 
-loginButton.addEventListener("click", handleLoginBtnClick);
-
-console.dir(loginInput);
+function handleLinkClick(event) {
+    console.log(event);
+    event.preventDefault();
+    alert("you Clicked");
+}
+blogLink.addEventListener("click", handleLinkClick);
